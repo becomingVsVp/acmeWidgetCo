d4b114e6aea405f29e2f7ef3325912c576880947
becomingVsVp <95556502+becomingVsVp@users.noreply.github.com>
2021-12-05 00:29:55 -0600

    Update autoload.php

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,4 +1,4 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    include_once '/Users/bbecker/vid1/classes/' . $class_name . '.class.php';
+    include_once '../classes/' . $class_name . '.class.php';
 });
